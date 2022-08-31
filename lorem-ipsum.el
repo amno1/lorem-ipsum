8c441c46afa8e48d269f72e4dcba799940319374
Arthur Miller <arthur.miller@live.com>
2022-08-31 16:39:51 +0200

    Converted and refactored some code to more emacs lisp like.

--- lorem-ipsum.el
+++ lorem-ipsum.el
@@ -2,9 +2,13 @@
 
 (require 'cl-lib)
 
-(defvar *prologue* "Lorem ipsum dolor sit amet, consectetur adipiscing elit")
+(defgroup lorem-ipsum
+  :prefix "lorem-ipsum-")
 
-(defvar *words*
+(defvar lorem-ipsum--prologue
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit")
+
+(defvar lorem-ipsum--table
   ["do" "eiusmod" "incididunt" "labore" "dolore" "aliqua" "erat" "bibendum"
     "venenatis" "condimentum" "nisi" "natoque" "penatibus" "magnis" "dis"
     "parturient" "montes" "aenean" "nam" "ante" "metus" "tempor" "nullam"
@@ -25,22 +29,29 @@
     "nibh" "tortor" "id" "aliquet" "lectus" "proin" "aliquam" "vestibulum"
     "blandit" "risus" "at" "ultrices" "mi" "facilisis" "sed" "morbi" "quis"
     "commodo" "odio" "cursus" "in" "hac" "habitasse" "platea" "dictumst"
-    "quisque" "sagittis" "purus" "sit"])
+    "quisque" "sagittis" "purus" "sit"]
+  "A table of words to use wehn producing the text.")
 
-(defun lorem-ipsum-word () (aref *words* (random (length *words*))))
+(defun lorem-ipsum-word ()
+  "Pick a random word from `lorem-ipsum--table'"
+  (aref lorem-ipsum--table (random (length lorem-ipsum--table))))
 
-(defun lorem-ipsum-words (n) (cl-loop repeat n collect (word)))
+(defun lorem-ipsum-words (n)
+  (let (words)
+    (dotimes (_i n) (push (lorem-ipsum-word) words))
+    words))
 
-(cl-defun lorem-ipsum-paragraph (&key (word-count 50) (prologue t))
+(defun lorem-ipsum--paragraph (&optional word-count prologue)
   (with-output-to-string
     (with-current-buffer standard-output
-      (let ((words-remaining word-count)
+      (let ((words-remaining (or word-count 50))
             (words-in-sentence (+ 2 (random 8)))
-            (capitalize t))
+            (capitalize t)
+            (prologue (or prologue t)))
         (cl-tagbody
          :start
          (cond (prologue
-                (princ *prologue*)
+                (princ lorem-ipsum--prologue)
                 (cl-decf words-in-sentence 3)
                 (setf capitalize nil))
                (t
@@ -59,7 +70,7 @@
                     (princ ", ")
                   (princ " "))))
          :output-word
-         (princ (if capitalize (capitalize (word)) (word)))
+         (princ (if capitalize (capitalize (lorem-ipsum-word)) (lorem-ipsum-word)))
          (setf capitalize nil)
          (cl-decf words-remaining)
          (cl-decf words-in-sentence)
@@ -67,10 +78,18 @@
          (go :end-paragraph)
          :end)))))
 
-(cl-defun lorem-ipsum-paragraphs (n &key (word-count 50) (prologue t))
-  (cl-loop with prologue = prologue
-        repeat n
-        collect (paragraph :word-count word-count :prologue prologue)
-        do (setf prologue nil)))
+(defun lorem-ipsum-paragraphs (n &optional word-count prologue)
+  (let ((text (lorem-ipsum--paragraph word-count prologue)))
+    (dotimes (_i (1- n))
+      (setq text (concat text "\n\n" (lorem-ipsum--paragraph word-count))))
+    text))
+
+(defun lorem-ipsum-paragraph ()
+  "Insert one paragraph of lorem ipsum words at point."
+  (interactive)
+  (let ((beg (point)))
+    (insert (lorem-ipsum-paragraphs 1 50 t))
+    (indent-region beg (point))
+    (fill-region beg (point) t)))
 
 (provide 'lorem-ipsum)
